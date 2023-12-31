aed4b8072e22f67a163efb1cb6fe3286a6125da2
nndakurnia <nandaka38@gmail.com>
2023-12-31 20:36:05 +0700

    add script to build n push image to GitHub Packages

---
+++ build_push_image.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+WAKTU=$(date '+%Y-%m-%d.%H')
+
+echo '[*] Building Docker Image'
+echo "[*] Tag $WAKTU"
+# build Docker image dari Dockerfile (name:order-service, tag:v1)
+docker build -t order-service:v1 .
+
+echo "[*] List Docker Images"
+# melihat list docker image di lokal
+docker images
+
+echo "[*] Mengubah nama image sesuai format GitHub Packages"
+# mengubah nama image sesuai format GitHub Packages
+docker image tag order-service:v1  ghcr.io/nndakurnia/order-service:v1
+
+echo "[*] Login ke GitHub Packages"
+# export PAT github di terminal dalam variabel CR_PAT
+# login ke github packages, pake stdin untuk menghindari pw ada di shell history atau log-files
+echo $CR_PAT | docker login ghcr.io -u nndakurnia --password-stdin
+
+echo "[*] Push image to GitHub Packages"
+# mengunggah image ke github packages
+docker push ghcr.io/nndakurnia/order-service:v1
